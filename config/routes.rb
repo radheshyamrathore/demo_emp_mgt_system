2060e789c5467ce657aafe1d6d5ee44f79296366
radheshyamrathore <rsrathore002@gmail.com>
2023-02-10 16:24:28 +0530

    generate new application and add_devise_gem

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'     
+end
+  # devise_for :users, controllers: {
+  #   sessions: 'users/sessions'
+  # }
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "users#index"
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
